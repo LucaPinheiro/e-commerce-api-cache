069705bfd6719f6c548c02440905f3522c6914d1
Luca Pinheiro Gomes <133619664+LucaPinheiro@users.noreply.github.com>
2024-12-15 01:57:59 -0300

    feat: repo products w sqlite

---
+++ src/models/sqlite/repository/interfaces/products_repository.py
@@ -0,0 +1,11 @@
+from abc import abstractmethod, ABC
+
+
+class ProductRepositoryInterface(ABC):
+
+    @abstractmethod
+    def find_product_by_name(self, product_name: str) -> tuple:
+        pass
+
+    def insert_product(self, name: str, price: float, quantity: int) -> None:
+        pass
